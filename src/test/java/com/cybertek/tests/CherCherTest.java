6e40a0eb72237d179d04ff2aeffe6809d910dff4
mhrrmtasyurek <mhrrmtasyurek@gmail.com>
2022-01-02 18:27:27 +0300

    first

---
+++ src/test/java/com/cybertek/tests/CherCherTest.java
@@ -0,0 +1,34 @@
+package com.cybertek.tests;
+
+import com.cybertek.utilities.WebDriverFactory;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+public class CherCherTest {
+
+    @BeforeMethod
+    public void setUp(){
+        WebDriver driver; // declare our reference for the object
+
+        driver = WebDriverFactory.getDriver("chrome"); // create the object
+            driver.manage().window().maximize();
+            // implicitly wait, this is going to be applied to whole test cases and elements
+            driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+            driver.get("https://chercher.tech/practice/explicit-wait-sample-selenium-webdriver");
+        }
+
+    @AfterMethod
+    public void tearDown(){
+        driver.close();
+
+    }
+    @Test
+    public void alertPresentTest(){
+
+    }
+
+}
