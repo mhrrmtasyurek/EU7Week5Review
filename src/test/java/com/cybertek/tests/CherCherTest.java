049c96261463227aff9e30313bfef0ce0363b4eb
mhrrmtasyurek <mhrrmtasyurek@gmail.com>
2022-01-02 18:43:49 +0300

    second

--- src/test/java/com/cybertek/tests/CherCherTest.java
+++ src/test/java/com/cybertek/tests/CherCherTest.java
@@ -1,7 +1,13 @@
 package com.cybertek.tests;
 
 import com.cybertek.utilities.WebDriverFactory;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -10,6 +16,7 @@ import java.util.concurrent.TimeUnit;
 
 public class CherCherTest {
     WebDriver driver; // declare our reference for the object
+    WebDriverWait wait;
     @BeforeMethod
     public void setUp(){
 
@@ -28,6 +35,14 @@ public class CherCherTest {
     }
     @Test
     public void alertPresentTest(){
+        WebElement initiateAlert= driver.findElement(By.id("alert"));
+        initiateAlert.click();
+
+         wait = new WebDriverWait(driver,10);
+        wait.until(ExpectedConditions.alertIsPresent());
+
+        Alert alert=driver.switchTo().alert();
+        alert.accept();
 
     }
 
