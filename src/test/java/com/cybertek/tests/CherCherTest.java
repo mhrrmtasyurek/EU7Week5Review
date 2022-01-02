529ce2d8be2c996e239161dce6f2443449cb2ba4
mhrrmtasyurek <mhrrmtasyurek@gmail.com>
2022-01-02 19:18:15 +0300

    test2

--- src/test/java/com/cybertek/tests/CherCherTest.java
+++ src/test/java/com/cybertek/tests/CherCherTest.java
@@ -8,6 +8,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -37,13 +38,24 @@ public class CherCherTest {
     public void alertPresentTest(){
         WebElement initiateAlert= driver.findElement(By.id("alert"));
         initiateAlert.click();
-
-         wait = new WebDriverWait(driver,10);
+        wait = new WebDriverWait(driver,10);
         wait.until(ExpectedConditions.alertIsPresent());
 
         Alert alert=driver.switchTo().alert();
         alert.accept();
 
+    }
+    @Test
+    public void disabledButtonTest(){
+        WebElement button = driver.findElement(By.id("disable"));
+         System.out.println("button.isEnabled() = " + button.isEnabled());
+    WebElement buttonInitiator = driver.findElement(By.id("enable-button"));
+    buttonInitiator.click();
+        wait = new WebDriverWait(driver,10);
+        wait.until(ExpectedConditions.elementToBeClickable(button));
+
+        System.out.println("button.isEnabled() = " + button.isEnabled()); // true
+        Assert.assertTrue(button.isEnabled(),"Verify the Button is enabled");
     }
 
 }
